fda92bc968dcc8a3d3940cea6612dd809200584b
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-17 13:05:29 -0700

    add play1round function for comparing player and ocomputer selection returning the result message. Also added the test input to test out the play1Round function

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
         return 'rock'
     }else if(number === 2){
         return 'paper'
-    }else{
+    }else{ //3
         return 'scissor'
     }
 
@@ -13,9 +13,29 @@ function getComputerChoice(){
 
 
 
-// function play1Round(playerSelection,compterSelection){
-//     // The function should take two parameters - the playerSelection and computerSelection - and then 
-//     // make it all lower so it's not case sensitive
+function play1Round(playerSelection,compterSelection){
+    // The function should take two parameters - the playerSelection and computerSelection - and then 
+    // make it all lower so it's not case sensitive
+    let p_select = playerSelection.toLowerCase();
+    let c_select = compterSelection.toLowerCase();
     
-//     // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// }
+    if ((p_select.includes('rock') && c_select.includes('scissor')) || (p_select.includes('scissor') && c_select.includes('paper')) || (p_select.includes('paper') && c_select.includes('rock'))){
+        // if win
+        return 'you win! ' + p_select + ' beats ' + c_select    
+    }else if(p_select==c_select){
+        // for tie
+        return 'Tied!'
+    }else{
+        return 'you loose! ' + c_select + ' beats ' + p_select
+    }
+    // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+}
+
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log('you chose: ' + playerSelection)
+console.log('I chose: ' + computerSelection)
+
+console.log(play1Round(playerSelection, computerSelection));
