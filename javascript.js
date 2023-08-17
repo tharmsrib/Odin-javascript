4fcda1dab68748d32a9666bc517fdf6b242a3143
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-17 13:46:01 -0700

    add game() function to play 5 rounds, also edit play1round function to output the score adding (1 for winner) or tie then output 0 score. game() display the selection, and score. If score reach 3 first then break and console.log(results)

--- javascript.js
+++ javascript.js
@@ -18,24 +18,56 @@ function play1Round(playerSelection,compterSelection){
     // make it all lower so it's not case sensitive
     let p_select = playerSelection.toLowerCase();
     let c_select = compterSelection.toLowerCase();
-    
+    let message = '';
+    let p_score =0; let c_score =  0;
     if ((p_select.includes('rock') && c_select.includes('scissor')) || (p_select.includes('scissor') && c_select.includes('paper')) || (p_select.includes('paper') && c_select.includes('rock'))){
         // if win
-        return 'you win! ' + p_select + ' beats ' + c_select    
+        message = 'you win this round! ' + p_select + ' beats ' + c_select;  
+        p_score++;  
     }else if(p_select==c_select){
         // for tie
-        return 'Tied!'
+        message = 'Tied!';
     }else{
-        return 'you loose! ' + c_select + ' beats ' + p_select
+        message = 'you loose this round! ' + c_select + ' beats ' + p_select;
+        c_score++;
     }
-    // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+    // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" & what score to add
+    return [message,[p_score,c_score]]
 }
 
+function game(){
+    let p_score = 0; let c_score = 0;
+    // let numRounds = prompt('How many rounds?');
+    let numRounds = 5;
+    for (let i_round = 0; i_round<numRounds;i_round++){
+        let playerSelection = prompt('Select Rock, Paper, or Scissor'); //player input
+        const computerSelection = getComputerChoice();  //computer selection
+        //display slection
+        console.log('you chose: ' + playerSelection)
+        console.log('I chose: ' + computerSelection)
+        //play & display
+        let results = play1Round(playerSelection, computerSelection)
+        let message = results[0];
+        let score_add = results[1];
+        console.log(message);
+        // console.log(results);
+        // add score
+        p_score += score_add[0];
+        c_score += score_add[1];
+        console.log(`Score(you,me): ${p_score} - ${c_score}`)
+        if(p_score===Math.ceil(numRounds/2) || c_score===Math.ceil(numRounds/2)){
+            
+            break;
+        }
 
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log('you chose: ' + playerSelection)
-console.log('I chose: ' + computerSelection)
+    }
+    if(p_score>c_score){
+        console.log('AHH, You Win!!!');
+    }else if(p_score<c_score){
+        console.log('Ha, You Loose!!!'); 
+    }else{
+        console.log('Tied!!!');
+    }
 
-console.log(play1Round(playerSelection, computerSelection));
+}
