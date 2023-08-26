f005f0b74d96e4ccbd1d82aee8c618413debfd1d
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-26 14:18:33 -0700

    add callback for tiles to change color when mouse hovered over

--- Sketch-project/js/javascript.js
+++ Sketch-project/js/javascript.js
@@ -1,13 +1,19 @@
 function createDivGrid(numGrid){
     let i = 1;
     numTiles = numGrid**2;
+    //grid container
+    grids.setAttribute('style',`grid-template-columns: repeat(${numGrid},1fr);`);
+
     while(i<numTiles+1){
         
 
-        let newDiv = createTileElement();
+        const newDiv = createTileElement();
         newDiv.classList.add(`tile${i}`);
         grids.appendChild(newDiv);
         // console.log(newDiv.classList.value);
+
+        //add event listen to each tile for collor change
+        newDiv.addEventListener('mouseover',changeTileColor);
 
         i++;
     }
@@ -22,7 +28,23 @@ function createTileElement(){
     return newDiv;
 }
 
+function changeTileColor(e){
+    e.target.classList.add('hoverTiles'); //or this.classList.add
+    // console.log(this.classList);
+}
+
+
+
+
 const container = document.querySelector('.container');
-const grids = document.querySelector('.grids');
+const grids = document.querySelector('.grid');
+let numGrid; //size of grid
 createDivGrid(5);
 console.log(container); 
+
+const newGridButton = document.querySelector('.newGridButton');
+newGridButton.addEventListener('click',function(){
+    console.log('hello');
+    //ask user for number
+})
+
