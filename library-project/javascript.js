576004cbf8a39b95c1b1a49c08c93d4a8db4f739
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-31 16:52:28 -0700

    add dialog forms and pop up when add new book button. only cancel and esc button works. Confirm button still let the form pass even if the form is empty

--- library-project/javascript.js
+++ library-project/javascript.js
@@ -1,10 +1,10 @@
 
 
-function Book(bookName){
+function Book(bookName,author,numPage){
     //the constructor
     this.name=bookName;
-    // this.author=author;
-    // this.numPage=numPage;
+    this.author=author;
+    this.numPage=numPage;
 }
 
 function addBookToLibrary(bookObj){
@@ -22,7 +22,7 @@ function displayBooks(){
     myLibrary.forEach(function (book){
         //create element and add class and text to it
         
-        console.log(book);
+        // console.log(book);
         const newBookDiv = createBookCardsElement(book);
         newBookDiv.classList.add(`card${i}`);       
         libraryDiv.appendChild(newBookDiv);
@@ -40,13 +40,67 @@ function createBookCardsElement(bookObj){
     a.textContent = bookObj.name;
     // a.textContent = 'hahaa';
     newDiv.appendChild(a);
-    console.log(newDiv)
+    // console.log(newDiv)
     return newDiv;
 }
 
 
 const myLibrary = [];
 const libraryDiv = document.querySelector('.library-container');
+const newBookButton = document.querySelector('.newBookBtn');
+const newBookDialog = document.getElementById('newBookDialog');
+const confirmBtn = document.getElementById('confirmBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+//newBook button opens dialog modally
+newBookButton.addEventListener("click",()=>{
+    newBookDialog.showModal();
+})
+
+
+// Prevent the "confirm" button from the default behavior 
+//of submitting the form, and close the dialog with 
+//the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+    
+    //create new book Object
+    let title = document.querySelector('#newBookTitle').value;
+    let author = document.querySelector('#newBookAuthor').value;
+    let numPage = document.querySelector('#newBookNumPage').value;
+    //create new book object
+    let newBook = new Book(title,author,numPage);
+    console.log(newBook);
+    confirmBtn.value = 'submit';
+    event.preventDefault(); // We don't want to submit this fake form
+    newBookDialog.close(newBook); // Have to send the select box value here.
+});
+
+cancelBtn.addEventListener("click", ()=>{
+    newBookDialog.close('cancel');
+})
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+newBookDialog.addEventListener("close", () => {
+    // outputBox.value =
+    //   favDialog.returnValue === "default"
+    //     ? "No return value."
+    //     : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+    //check if cancel or confirm button pushed
+    // console.log(newBookDialog.returnValue)
+    if(newBookDialog.returnValue==='default'){
+        console.log('no return value')
+    }else if(newBookDialog.returnValue==='cancel'){
+        console.log('cancel')
+    }else if(newBookDialog.returnValue==='submit'){
+        //add new book to library array
+        // addBookToLibrary(newBookDialog.returnValue);
+        console.log('new book add')
+        console.log(myLibrary)
+        //display new one
+    }
+    
+
+});
+
 //manual book
 let bookA = new Book('a');
 let bookB = new Book('b');
