50ddd7ec820b7d0840bc9dc20cae30985e4d8935
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-31 13:03:54 -0700

    add function to create book cards per book object  and it's content like names. Add displaybooks functions to loop through myLibrary array of jects to create newe book divs cards and append the card divs to the container

--- library-project/javascript.js
+++ library-project/javascript.js
@@ -1,10 +1,10 @@
 
 
-function Book(bookName,author,numPage){
+function Book(bookName){
     //the constructor
     this.name=bookName;
-    this.author=author;
-    this.numPage=numPage;
+    // this.author=author;
+    // this.numPage=numPage;
 }
 
 function addBookToLibrary(bookObj){
@@ -17,8 +17,42 @@ function addBookToLibrary(bookObj){
 // them in some sort of table, or each on their own 
 // “card”. It might help for now to manually add
 //  a few books to your array so you can see the display.
+function displayBooks(){
+    let i =0;
+    myLibrary.forEach(function (book){
+        //create element and add class and text to it
+        
+        console.log(book);
+        const newBookDiv = createBookCardsElement(book);
+        newBookDiv.classList.add(`card${i}`);       
+        libraryDiv.appendChild(newBookDiv);
+        i++;
+
+    })
+}
+
+function createBookCardsElement(bookObj){
+    //create div element and add book content to it
+    let newDiv = document.createElement('div');
+    let a = document.createElement('a');
+    newDiv.classList.add('bookCard');
+    a.classList.add('bookContent');
+    a.textContent = bookObj.name;
+    // a.textContent = 'hahaa';
+    newDiv.appendChild(a);
+    console.log(newDiv)
+    return newDiv;
+}
 
 
 const myLibrary = [];
-const libraryDiv = document.querySelector('library-container');
+const libraryDiv = document.querySelector('.library-container');
+//manual book
+let bookA = new Book('a');
+let bookB = new Book('b');
+addBookToLibrary(bookA);
+addBookToLibrary(bookB);
+displayBooks();
+
+
 
